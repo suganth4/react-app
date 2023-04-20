39959152c5ea63dc2671dcb9649d285b6fc1c597
suganth-skill-lync <suganth.c@skill-lync.com>
2023-04-20 13:35:36 +0530

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div>
+      <div>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <p>Astonishing features requires worry-free security</p>
+          <hr />
+          <p>1 Year Free Mobile Security</p>
+        </div>
+        <div>
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Card
