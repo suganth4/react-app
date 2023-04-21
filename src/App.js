b00fe6d41e40cbaafa3e89a1ff1041779fc95499
suganth-skill-lync <suganth.c@skill-lync.com>
2023-04-21 22:35:25 +0530

    code updated

--- src/App.js
+++ src/App.js
@@ -1,57 +1,28 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import Card from './components/Card';
-import { fetchData } from './utils';
-
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
+import { fetchlivescore } from './utils';
 function App() {
 
-  const [width, setWidth] = useState(window.innerWidth);
-  const breakpoint = 700;
   useEffect(() => {
-    const handleResizeWindow = () => setWidth(window.innerWidth);
-    window.addEventListener("resize", handleResizeWindow);
-    return () => {
-      window.removeEventListener("resize", handleResizeWindow);
-    };
+    fetchlivescore().then((res) => {
+      setData(res.Stages[0].Events[0].ECo)
+    })
   }, []);
 
-  const [data, setData] = useState("")
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1
-  };
-  useEffect(() => {
-    fetchData().then((res) => {
-      console.log("asd", res)
-      setData(res)
+  setInterval(function () {
+    fetchlivescore().then((res) => {
+      setData(res.Stages[0].Events[0].ECo)
     })
-  }, [])
+  }, 1000 * 60)
+
+  const [data, setData] = useState("")
 
   if (data == "") {
     return <>loading</>
   }
   return (
     <div className="App">
-
-
-      {width < 450 ? <> <div className="header-mobile" ><img className="header-img-mobile" src={data.logo} /> </div> <Slider {...settings}>
-        {data.features.map((value, index) => {
-          return (
-            <Card data={value} index={index} />
-          )
-        })}
-      </Slider> </> : <> <div className="header" ><img className="header-img" src={data.logo} /> </div> {data.features.map((value, index) => {
-        return (
-          <Card data={value} index={index} />
-        )
-      })}</>}
-
+      {data}
     </div>
   );
 }
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,11 @@
-export function fetchData () {
-    return fetch("https://krds-assignment.github.io/aoc/api-assets/data.json").then((res)=> res.json())
+export function fetchlivescore() {
+    return fetch("https://livescore6.p.rapidapi.com/matches/v2/list-live?Category=cricket&&Timezone=5.5", {
+        headers: {
+            'X-RapidAPI-Key': 'hPq1NScczpcJXMwHcP3TbJp7yn3WkJio',
+            'X-RapidAPI-Host': 'livescore6.p.rapidapi.com'
+          },
+        
+    }).then((res) => {
+        return res.json()
+    })
 }
